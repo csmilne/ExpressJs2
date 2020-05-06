62c0932cd109e9903c294cf316a0fb3fd693cb43
csmilne <35082425+csmilne@users.noreply.github.com>
2020-05-05 21:43:17 -0400

    bodyParser()

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/',(req, res, next) => {
-    console.log('In the next middleware');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product',(req, res, next) => {
-    console.log('In add-product');
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/product',(req, res, next) => {
+app.post('/product',(req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
 
 app.use('/',(req, res, next) => {
-    console.log('In the next middleware');
     res.send('<h1>Hello from express</h1>');
 });
 
