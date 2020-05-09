ac3a9e1405ab99e4e1ceaf23ab31453e39821f4f
csmilne <35082425+csmilne@users.noreply.github.com>
2020-05-09 13:03:18 -0400

    addHelperUtil()

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
-
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.sendfile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendfile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
